8c4de63c407ede263ef1ad8e2f10b636eb53be1e
bida syafaat <abisyabida@gmail.com>
2024-08-31 15:51:29 +0000

    modified: myApp

--- myApp.js
+++ myApp.js
@@ -7,6 +7,10 @@ console.log("Hello World")
 //app.get("/", (req, res) => {
 //    res.send("Hello Express")
 //})
+
+//#4
+app.use("/public", express.static(__dirname + "/public"))
+
 //#3
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
