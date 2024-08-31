2c76ac4dbc1cc600b702f275db4f77a290ca653c
bida syafaat <abisyabida@gmail.com>
2024-08-31 10:38:09 +0000

    modified: myApp

--- myApp.js
+++ myApp.js
@@ -1,7 +1,16 @@
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
-
-
+//#1
+console.log("Hello World")
+//#2
+//app.get("/", (req, res) => {
+//    res.send("Hello Express")
+//})
+//#3
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+})
 
 
 
