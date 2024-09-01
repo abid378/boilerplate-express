cd76e3dae7e28f6c8bf9d9573b931e647a70a310
bida syafaat <abisyabida@gmail.com>
2024-09-01 04:40:01 +0000

    modified: myApp

--- myApp.js
+++ myApp.js
@@ -16,6 +16,21 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 })
 
+//#5
+//app.get("/json", (req, res) => {
+//    res.json({
+//        "message": "Hello json"
+//    })
+//})
+
+//#6
+app.get("/json", (req, res) => {
+   if ( process.env["MESSAGE_STYLE"] == "uppercase"){
+    res.json({"message": "HELLO JSON"})
+   } else {
+    res.json({"message": "hello json"})
+   }
+  })
 
 
 
